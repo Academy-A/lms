ae77ec1b50367f1d158cd422205b753054a49c74
Sergey Natalenko <sergey.natalenko@mail.ru>
2025-03-02 00:05:58 +0300

    Update config

--- lms/application/security.py
+++ lms/application/security.py
@@ -4,4 +4,4 @@ from os import environ
 
 @dataclass(frozen=True, slots=True, kw_only=True)
 class SecurityConfig:
-    secret_key: str = field(default_factory=lambda: environ["API_SECRET_KEY"])
+    secret_key: str = field(default_factory=lambda: environ["APP_SECURITY_SECRET_KEY"])
